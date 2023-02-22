3913b337e798002a98e0839d1a00cbbb8cfa7133
PabVF23 <60737771+PabVF23@users.noreply.github.com>
2023-02-22 16:10:37 +0100

    Initial commit

---
+++ src/test/java/com/uniovi/sdi2223entrega133/Sdi2223Entrega133ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2223entrega133;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2223Entrega133ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
